aa127f90a86a33fd251885862d072cb9417aa19c
Elena <elenaharan@gmail.com>
2024-11-11 07:48:26 +0000

    implement tours endpoint

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 //define routes
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Noatours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Noatours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json(tours);
 });
 
 const port = 3000;
