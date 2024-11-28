fbaa2e4323274ac698eb1c29efe83c9cea752f89
Elena <elenaharan@gmail.com>
2024-11-28 08:01:12 +0000

    finish connecting MongoDB with the app

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const app = require('./app');
 
