d050bfa3771b94412df459588be63e9ee5ecac75
Elena <elenaharan@gmail.com>
2024-11-30 13:27:35 +0000

    refactor to follow the MVC architecture

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,24 +1,8 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-
-//middleware functions
-exports.checkId = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    //it is important to return this function, so that this middleware doesn't hit next() and progress calling the next middleware
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  next();
-};
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+// );
 
 exports.checkBody = (req, res, next) => {
   console.log(req.body);
@@ -40,10 +24,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -52,46 +36,32 @@ exports.getTourById = (req, res) => {
   console.log(req.params);
 
   //multiplying by 1 converts a string into a number
-  const id = req.params.id * 1;
+  // const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+  // const tour = tours.find((el) => el.id === id);
+  // // if (id > tours.length) {
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
+  // }
 
   res.status(200).json({
     status: 'success',
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
-
 
 //out of the box, express does not put data on the request
 //for that we need to use middleware
 //declare: app.use(express.json()); at the top
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+  });
 };
 
 exports.updateTour = (req, res) => {
