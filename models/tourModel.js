3f7bd18487a200e6e758c6980c37b1c312c6e7a9
Elena <elenaharan@gmail.com>
2024-12-23 07:44:50 +0000

    add example of validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxLength: [40, 'A tour must have less or equal than 40 characters'],
       minLength: [10, 'A tour must have 10 or more characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
