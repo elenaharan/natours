0ec2f4ff93c8fad23bde1acfe4b81ee7f90c0ff3
Elena <elenaharan@gmail.com>
2024-12-24 08:06:41 +0000

    remove unused middleware

--- app.js
+++ app.js
@@ -12,15 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-//we need to pass in a function that we want to add to our middleware stack
-//by adding next as the third argument, express knows that we are defining a middleware
-//the middleware functions must come before route handlers
-//because the route handler would end the req-res cycle and out middleware wouldn't be executed
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
