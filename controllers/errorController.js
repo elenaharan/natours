6251f80c906c8d8c3972f78a9016e1bb1a17c533
Elena <elenaharan@gmail.com>
2024-12-30 11:22:54 +0000

    transform CastError in production message into a user-friendly message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: is ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -27,5 +34,11 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
-  else if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
+  else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err, name: err.name };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
