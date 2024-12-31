1301d57784a406664e374b45aaf9e7564baaaca6
Elena <elenaharan@gmail.com>
2024-12-31 10:28:37 +0000

    handle duplicate db fields err

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/"([^"]*)"/g)[0];
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -38,6 +45,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err, name: err.name };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 110000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
