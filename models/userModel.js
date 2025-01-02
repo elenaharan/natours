be17fab01c0d4b78778131955b0e3d19e8814b2f
Elena <elenaharan@gmail.com>
2025-01-02 19:47:49 +0000

    schema

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email.'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: [8, 'A password must have 8 characters or more.'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please, confirm your password.'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
