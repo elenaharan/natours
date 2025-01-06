9f02d6225b6cb8bc4d3bd75eadbdb79f46b15fc1
Elena <elenaharan@gmail.com>
2025-01-06 07:53:38 +0000

    implement user login in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -21,5 +22,25 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1) check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  //2) check if user exists and password correct
+  const user = await User.findOne({ email }).select('+password');
+  console.log(user);
+
+  //3) if everything ok, send token
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
