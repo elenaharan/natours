aa1f7d956f2051d96876660d2d7a26241c80989a
Elena <elenaharan@gmail.com>
2025-01-06 07:54:08 +0000

    implement get all users from the DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,21 @@
-const fs = require('fs');
+// const fs = require('fs');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
-);
+// const users = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/users.json`);
+// );
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
   res.status(200).json({
     status: 'success',
     data: {
       users,
     },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
