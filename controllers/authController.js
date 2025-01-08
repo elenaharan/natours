fe8ab4abcf423b63a6664acfdc4cf464bad3a046
Elena <elenaharan@gmail.com>
2025-01-08 08:42:04 +0000

    start implementing middleware to check if user logged in

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,3 +51,28 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1) Get the token and check if it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401),
+    );
+  }
+  //2)Verification token
+
+  //3)check if user still exists
+
+  //4)check if user changed password after the JWT was issued
+
+  next();
+});
