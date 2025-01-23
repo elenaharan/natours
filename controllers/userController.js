67631ddc9f44e37e978fd7054e39f01a1301b389
Elena Haran <elenaharan@gmail.com>
2025-01-23 07:50:32 +0000

    begin implementing updateMe route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 // const fs = require('fs');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // const users = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/users.json`);
@@ -14,6 +15,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //create an error if the user POSTs the password
+  if (req.body.password || req.body.passwordConfirm)
+    next(
+      new AppError(
+        'This route is not for password updates! Please use /updateMyPassword',
+        400,
+      ),
+    );
+
+  //update user doc: we can use findByIdAndUpdate because we are not dealing with updating sensitive data like password
+  //new:true => returns the new updated object instead of the old one
+  //runValidators: true => check if supplied fields are correct
+  //we will need to filter the body so that it only contains certain fields that are allowed to be updated
+  //for ex, if body.role = 'admin', this should not be allowed
+  // const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = User.findByIdAndUpdate(req.user.id, x, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
   });
 });
 
