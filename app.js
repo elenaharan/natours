cd6ea050b30e87aea2558aecce75569a979aa8ed
Elena Haran <elenaharan@gmail.com>
2025-01-30 07:24:16 +0000

    add middleware to sanitize data against noSQL query injection and XSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,6 +41,12 @@ app.use(
     limit: '10kb',
   }),
 );
+
+//DATA SANITIZATION against NoSQL query injection
+app.use(mongoSanitize());
+
+//DATA SANITIZATION against XSS (Cross-site scripting attacks)
+app.use(xss());
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
