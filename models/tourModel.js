c0806a93216c51616ac10c9d6683be82385d64b4
Elena Haran <elenaharan@gmail.com>
2025-02-04 09:13:50 +0000

    move populate into middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -159,6 +159,15 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v',
+  });
+
+  next();
+});
+
 //AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({
