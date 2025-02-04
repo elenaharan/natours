a97134d70f635f5c0fb62c5640fe64f0d20bf3e8
Elena Haran <elenaharan@gmail.com>
2025-02-04 09:05:05 +0000

    use populate method to embed guides info into a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,7 +35,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTourById = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate({
+    path: 'guides',
+    select: '-__v',
+  });
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
