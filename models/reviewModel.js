590d12e124216938ed84f15047a5b77d6bd81d53
Elena Haran <elenaharan@gmail.com>
2025-02-05 08:08:19 +0000

    create review model

---
+++ models/reviewModel.js
@@ -0,0 +1,42 @@
+// review / rating/ createdAt/ ref to Tour/ ref to User
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review field cannot be empty'],
+      maxLength: [200, 'Your review exceeded maximum length'],
+      minLength: [2, 'Your review is too short.'],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  //specify that virtual properties will show up in JSON and object output
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
