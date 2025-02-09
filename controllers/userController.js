0d81104dca56a11b212f86deffbc7b1b7cfc2495
Elena Haran <elenaharan@gmail.com>
2025-02-09 16:55:59 +0000

    update controllers by using factory function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -90,9 +91,14 @@ exports.updateUser = (req, res) => {
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
-  });
-};
+// exports.deleteUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'this route is not yet defined',
+//   });
+// };
+
+exports.deleteUser = factory.deleteOne(User);
+//DO NOT USE TO UPDATE PASSWORD
+exports.updateUser = factory.updateOne(User);
+
