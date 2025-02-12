a277c92ee8368efb40e65375433a5d552b135173
Elena Haran <elenaharan@gmail.com>
2025-02-12 06:36:02 +0000

    expose all tours to unauth users

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,7 +22,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
+  .get(tourController.getAllTours)
   .post(tourController.createTour);
 
 router
