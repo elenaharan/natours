2fe3ada678d0d4d06997ed083a3ab524524cdbf5
Elena Haran <elenaharan@gmail.com>
2025-02-12 06:46:39 +0000

    use protect middleware on most user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,19 +8,13 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword,
-);
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUserById,
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUserById);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
