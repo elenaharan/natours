42d1832bb328d8f14627ade33143a54fb7a6a209
Elena Haran <elenaharan@gmail.com>
2025-02-12 06:32:59 +0000

    implement get current user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,12 @@ router.patch(
   authController.protect,
   authController.updatePassword,
 );
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUserById,
+);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
