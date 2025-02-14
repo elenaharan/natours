8a981b3d1adb56e6104dba0ccc99d11181d9d20f
Elena Haran <elenaharan@gmail.com>
2025-02-14 09:22:00 +0000

    add indexes to improve read performance

--- models/tourModel.js
+++ models/tourModel.js
@@ -116,6 +116,10 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
+//compound indexing below will also cover price and ratingsAverage individually
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
