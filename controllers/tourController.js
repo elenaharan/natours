a341cfcc7e6ae7c0abd0ddaf841951b43a5267fa
Elena Haran <elenaharan@gmail.com>
2025-02-21 08:01:32 +0000

    agg controller for the new route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -131,3 +131,38 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longtitude in the format lat, lng.',
+        400,
+      ),
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    //geoNear should always be the first in the aggregation pipeline
+    //geoNear requires that at least one of the fields contains geospacial index
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
