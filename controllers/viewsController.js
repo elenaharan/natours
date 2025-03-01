14087ed0bd077f27cd90637da6cf3937977158c1
Elena Haran <elenaharan@gmail.com>
2025-03-01 13:48:44 +0000

    add controller for overview route

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  //2. Build template
+  //3. Render that template using the data from step 1
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours: tours,
   });
-}
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
   });
-}
+};
