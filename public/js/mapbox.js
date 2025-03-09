ee77d1898a269cf62f8d68a45a5c5860ef363c09
Elena <elenaharan@gmail.com>
2025-03-09 10:41:00 +0000

    move script at the end of tour template to run after DOM mounted

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
 
 console.log('Hello from the client side.');
-const locations = document.getElementById('map').dataset.locations;
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
