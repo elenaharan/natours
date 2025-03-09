385c64dedcc67a21a1c9f64230446d3e4fe8aa63
Elena <elenaharan@gmail.com>
2025-03-09 11:09:39 +0000

    include mapbox sdk

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,5 +1,12 @@
 /* eslint-disable */
-
 console.log('Hello from the client side.');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
