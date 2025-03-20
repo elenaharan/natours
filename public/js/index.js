01a9c69104eb3653bc1dd580996f06a14a5b1288
Elena Haran <elenaharan@gmail.com>
2025-03-20 10:10:29 +0000

    refactor index and login

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+import '@babel/polyfill'
+import { displayMap } from './mapbox'
+import { login } from './login'
+
+//DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+//VALUES
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+//DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    login(email, password)
+  })
+}
