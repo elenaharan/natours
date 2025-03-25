2beaabff60aa1f952f841e4e959d28cec274a7d1
Elena Haran <elenaharan@gmail.com>
2025-03-25 09:15:40 +0000

    add modifier to login form class

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
-import '@babel/polyfill'
-import { displayMap } from './mapbox'
-import { login, logout } from './login'
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login, logout } from './login';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
-const logOutButton = document.querySelector('.nav__el--logout')
+const loginForm = document.querySelector('.form--login');
+const logOutButton = document.querySelector('.nav__el--logout');
 
 //DELEGATION
 if (mapBox) {
@@ -18,8 +18,8 @@ if (loginForm) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    login(email, password)
-  })
+    login(email, password);
+  });
 }
 
-if (logOutButton) logOutButton.addEventListener('click', logout)
+if (logOutButton) logOutButton.addEventListener('click', logout);
