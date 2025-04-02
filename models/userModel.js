ca9b211c4becc44bbaaec60ac9717540d7c6074c
Elena Haran <elenaharan@gmail.com>
2025-04-02 09:24:15 +0100

    add default img to new user photo field

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,10 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email.'],
   },
-  photo: String,
+  photo: {
+    type: String,
+    dafault: 'default.jpg',
+  },
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
@@ -74,7 +77,7 @@ userSchema.methods.correctPassword = async function (
   userPassword,
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
-}
+};
 
 userSchema.pre(/^find/, function (next) {
   this.find({ active: { $ne: false } });
