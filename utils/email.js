7316dbbf144068900b08ef39cf8846b1f239cfaf
Elena Haran <elenaharan@gmail.com>
2025-05-05 08:13:01 +0100

    add methods and outline send method

--- utils/email.js
+++ utils/email.js
@@ -7,30 +7,45 @@ module.exports = class Email {
     this.url = url;
     this.from = `Elena Haran <${process.env.EMAIL_FROM}>`;
   }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  //send the actual email
+  send(template, subject) {
+    // 1) render html based on a pug template
+
+    // 2) define email options
+    const mailOptions = {
+      from: 'Elena Haran <hello@elena.io>',
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+      //html: we can specify the HTML property or leave it as text
+    };
+
+    //3) create transport & send email
+  }
+
+  sendWelcome() {
+    this.send('welcome', 'Welcome to the Natours Family!');
+  }
 };
 
 const sendEmail = async (options) => {
-  //1) create a transporter - this is what will actually send the email
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  //2) define email options
-  const mailOptions = {
-    from: 'Elena Haran <hello@elena.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html: we can specify the HTML property or leave it as text
-  };
-
-  //3) send email
+  //send email
   await transporter.sendMail(mailOptions);
 };
-
-module.exports = sendEmail;
