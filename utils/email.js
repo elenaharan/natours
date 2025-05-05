efbc36290f30790fc5a64e5f9d3176b2882f6947
Elena Haran <elenaharan@gmail.com>
2025-05-05 07:57:33 +0100

    use env var

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = 'Elena Haran <hello@elena.io>';
+    this.from = `Elena Haran <${process.env.EMAIL_FROM}>`;
   }
 };
 
