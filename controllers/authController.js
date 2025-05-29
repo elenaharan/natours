7ec2472696396405adf97c83d425bcaa3af5b07a
Elena Haran <elenaharan@gmail.com>
2025-05-29 07:05:50 +0100

    minor cleanup

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -36,15 +36,11 @@ const createAndSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  //   passwordChangedAt: req.body.passwordChangedAt,
-  //   role: req.body.role,
-  // });
   const newUser = await User.create(req.body);
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
+
   createAndSendToken(newUser, 201, res);
 });
 
@@ -70,10 +66,10 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.logout = (req, res) => {
   res.cookie('jwt', 'logged out', {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
-  })
-  res.status(200).json({ status: 'success' })
-}
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
 
 exports.protect = catchAsync(async (req, res, next) => {
   //1) Get the token and check if it exists
@@ -122,7 +118,10 @@ exports.isLoggedIn = async (req, res, next) => {
   try {
     if (req.cookies.jwt) {
       //verify the token
-      const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET,
+      );
 
       // Check if user still exists
       const currentUser = await User.findById(decoded.id);
@@ -144,20 +143,20 @@ exports.isLoggedIn = async (req, res, next) => {
   } catch (err) {
     return next();
   }
-  next()
+  next();
 };
 
 exports.restrictTo =
   (...roles) =>
-    (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return next(
-          new AppError('You do not have permission to perform this action', 403),
-        );
-      }
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
 
-      next();
-    };
+    next();
+  };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   //1) Get user based on posted email
@@ -176,11 +175,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a Patch request with your new password and passwordConfirm to ${resetURL}. /n If you didn't forget your password, please ignore this email.`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
