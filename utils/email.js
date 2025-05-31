c570a9e7c9080034470e0a7a4a5757ac9bb0213e
Elena Haran <elenaharan@gmail.com>
2025-05-31 20:45:00 +0100

    change name of env var

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Elena Haran <${process.env.EMAIL_FROM}>`;
+    this.from = `Elena Haran <${process.env.SENDGRID_EMAIL_FROM}>`;
   }
 
   newTransport() {
