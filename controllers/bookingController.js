f40d88a9400e36bd03f46ec9bef3def40822d47a
Elena Haran <elenaharan@gmail.com>
2025-06-01 11:37:20 +0100

    lay out steps for the controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,1 +1,10 @@
-exports.getCheckoutSession = (req, res, next) => {};
+// const Tour = require('../models/tourModel');
+// const AppError = require('../utils/appError');
+// const catchAsync = require('../utils/catchAsync');
+// const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = (req, res, next) => {
+  // 1) Get the currently booked tour by id from the DB
+  // 2) Create the checkout session
+  // 3) Send the checkout session to the FE in the response
+};
